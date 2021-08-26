2482e537a34510ec7b77a63113883093a8368885
Yingbei-Tong <tybalex@gmail.com>
2021-08-27 05:04:39 +0800

    minor fix for logger becomes f-string

--- nulog-inference-service/start-nulog-inference.py
+++ nulog-inference-service/start-nulog-inference.py
@@ -136,13 +136,13 @@ def s3_setup(s3_client):
     # Function to set up a S3 bucket if it does not already exist.
     try:
         s3_client.meta.client.head_bucket(Bucket=S3_BUCKET)
-        logger.debug("{S3_BUCKET} bucket exists")
+        logger.debug(f"{S3_BUCKET} bucket exists")
     except ClientError as e:
         # If a client error is thrown, then check that it was a 404 error.
         # If it was a 404 error, then the bucket does not exist.
         error_code = e.response["Error"]["Code"]
         if error_code == "404":
-            logger.warning("{S3_BUCKET} bucket does not exist so creating it now")
+            logger.warning(f"{S3_BUCKET} bucket does not exist so creating it now")
             s3_client.create_bucket(Bucket=S3_BUCKET)
     return True
 
